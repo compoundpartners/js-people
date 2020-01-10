a147234384158a9e04d18ed26b8da7251a863659
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2020-01-10 11:57:12 +0100

    fix location filter

--- aldryn_people/__init__.py
+++ aldryn_people/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.6.15'
+__version__ = '2.6.16'
 
 DEFAULT_APP_NAMESPACE = 'js_people'
--- aldryn_people/filters.py
+++ aldryn_people/filters.py
@@ -64,7 +64,7 @@ class PeopleFilters(CustomFilterMixin, django_filters.FilterSet):
     service = django_filters.ModelChoiceFilter('services', label='service', queryset=Service.objects.published().exclude(**ADDITIONAL_EXCLUDE.get('service', {})).order_by('translations__title'))
     group = django_filters.ModelChoiceFilter('groups', label='group', queryset=models.Group.objects.exclude(**ADDITIONAL_EXCLUDE.get('group', {})).order_by('translations__name'))
     letter = django_filters.CharFilter('last_name', 'istartswith')
-    location = django_filters.ModelChoiceFilter('locations', label='location', queryset=Location.objects.published().exclude(**ADDITIONAL_EXCLUDE.get('location', {})).order_by('translations__name'))
+    location = django_filters.ModelChoiceFilter('location', label='location', queryset=Location.objects.published().exclude(**ADDITIONAL_EXCLUDE.get('location', {})).order_by('translations__name'))
 
     class Meta:
         model = models.Person
